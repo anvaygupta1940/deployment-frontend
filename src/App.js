b2208e1afcbcc381595f5d776b45ef2f38cd94ad
anvaygupta1940 <guptaanvay1940@gmail.com>
2025-04-20 15:18:29 +0530

    adding redisss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/SignUp";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import api from "./api";
 
 function App() {
+  const [me, setMe] = useState(null);
+
+  useEffect(() => {
+    console.log("inside the use effect in the frontend");
+    api.get("/me")
+      .then(res => {
+        console.log("res>>", res);
+        setMe(res.data)
+      })
+      .catch(() => {
+        // alert("Not logged in");
+      });
+  }, []);
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar me={me} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,45 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../api";
 
-const Navbar = () => (
-    <nav>
-        <Link to="/">Home</Link> | <Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
-    </nav>
-);
+
+const Navbar = ({ me }) => {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await api.post("/logout",);
+            alert("Logged out!");
+            navigate("/login");
+
+        } catch (error) {
+            console.error("Error during logout:", error);
+            alert(error?.response?.data || "Error logging out!!");
+        }
+    };
+
+
+
+
+
+    return (
+        <nav>
+            <Link to="/" style={{ color: "blue", cursor: "pointer", textDecoration: "none" }}>Home</Link> |
+            {!me && (
+                <>
+                    <Link to="/login" style={{ color: "blue", cursor: "pointer", textDecoration: "none" }}>Login</Link> |
+                </>
+            )}
+            {!me && (
+                <>
+                    <Link to="/signup" style={{ color: "blue", cursor: "pointer", textDecoration: "none" }}>Signup</Link> |
+                </>
+            )}
+            {me && <button onClick={handleLogout} style={{ background: "none", border: "none", color: "blue", cursor: "pointer", fontSize: "20px" }}>
+                Logout
+            </button>}
+        </nav>
+    );
+};
 
 export default Navbar;
